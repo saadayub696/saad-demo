503d0b47b7476500920f2d99da57ef26bac20734
Saad Ayub <saadayub696@gmail.com>
2025-07-08 11:51:59 +0500

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,20 @@
+print ("Exercise 1")
+print("To create a calculator")
+a=7
+b=3
+print("The value of a is:",a,"And the value of b is", b)
+print("Addition of two numbers is:")
+add=a+b
+print(add)
+print("Subtraction of two numbers is:")
+sub=a-b
+print(sub)
+print("Multiplication of two numbers is:")
+mul=a+b
+print(mul)
+print("Division of two numbers is:")
+div=a/b
+print(div)
+print("Modulus of two numbers is:")
+mod=a%b
+print(mod)
